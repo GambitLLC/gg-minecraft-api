53625b4d5754c9dde23042e42038bb965cf80965
bendgk <benkosten@gmail.com>
2022-12-08 16:57:33 +0000

    changing everything for azure vm's

--- profile-server/main.go
+++ profile-server/main.go
@@ -27,7 +27,7 @@ func main() {
 
 	// -- connect to redis --
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
+		Addr:     "127.0.0.1:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
